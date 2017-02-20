b82b5ce519097eb7b67111a3d2a8234c2355a45f
rad19 <rad19@up.edu>
2017-02-19 17:00:32 -0800

    alternateCaps

--- app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
+++ app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
@@ -38,7 +38,7 @@ public class TextModActivity extends ActionBarActivity {
     private Button reverseButton;
     private Button copyButton;
     private int myPosition ;
-
+    private Button alternateCaps;
 
     private Button upperCase;
     private Button clear;
@@ -55,6 +55,8 @@ public class TextModActivity extends ActionBarActivity {
 
         lowerCase = (Button) findViewById(R.id.lowerButton);
         lowerCase.setOnClickListener(new lowerCaseButtonListener());
+        alternateCaps = (Button) findViewById(R.id.alternateCapsButton);
+        alternateCaps.setOnClickListener(new alternateCapsButtonListener());
 
 
 
@@ -221,5 +223,20 @@ public class TextModActivity extends ActionBarActivity {
         }
     }
 
+    private class alternateCapsButtonListener implements View.OnClickListener {
+        public void onClick(View v) {
+            String s1 = editText.getText().toString();
+            s1 = s1.toLowerCase();
+            String s2 = "";
+            for(int i = 0; i < s1.length(); i++) {
+                if(i % 2 == 0) {
+                    s2 += s1.substring(i, i+1).toUpperCase();
+                } else {
+                    s2 += s1.substring(i, i+1);
+                }
+            }
+            editText.setText(s2);
+        }
+    }
 
 }
